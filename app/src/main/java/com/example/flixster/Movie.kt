7a3e7a6175e1ce08c730a712b17491c378b3644d
Brian Balthazar <40572853+eleferrets@users.noreply.github.com>
2022-09-21 14:09:46 -0400

    Added the details page with the intent and the youtube information

--- app/src/main/java/com/example/flixster/Movie.kt
+++ app/src/main/java/com/example/flixster/Movie.kt
@@ -1,18 +1,28 @@
 package com.example.flixster
 
+import android.os.Parcelable
+import kotlinx.parcelize.IgnoredOnParcel
+import kotlinx.parcelize.Parcelize
 import org.json.JSONArray
 
 // Model for delegation from the main activity
+@Parcelize
 data class Movie(
     val movieId: Int,
+    val voteAverage: Double,
+    val adult: Boolean,
+    val releaseDate: String,
     // Devs don't care about the posterpath
     private val posterPath: String,
     private val backdropPath: String,
     val title: String,
     val overview: String,
-) {
+) : Parcelable {
     // Devs care about the full url
+    @IgnoredOnParcel
     val posterImageUrl = "https://image.tmdb.org/t/p/w342/$posterPath"
+
+    @IgnoredOnParcel
     val backdropImageUrl = "https://image.tmdb.org/t/p/w300/$backdropPath"
 
     companion object {
@@ -24,6 +34,9 @@ data class Movie(
                 movies.add(
                     Movie(
                         movieJson.getInt("id"),
+                        movieJson.getDouble("vote_average"),
+                        movieJson.getBoolean("adult"),
+                        movieJson.getString("release_date"),
                         movieJson.getString("poster_path"),
                         movieJson.getString("backdrop_path"),
                         movieJson.getString("title"),
--- app/src/main/java/com/example/flixster/MovieAdapter.kt
+++ app/src/main/java/com/example/flixster/MovieAdapter.kt
@@ -1,6 +1,7 @@
 package com.example.flixster
 
 import android.content.Context
+import android.content.Intent
 import android.content.res.Configuration
 import android.util.Log
 import android.view.LayoutInflater
@@ -8,10 +9,12 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 
 private const val TAG = "MovieAdapter"
+const val MOVIE_EXTRA = "MOVIE_EXTRA"
 
 class MovieAdapter(private val context: Context, private val movies: List<Movie>) :
     RecyclerView.Adapter<MovieAdapter.ViewHolder>() {
@@ -33,10 +36,16 @@ class MovieAdapter(private val context: Context, private val movies: List<Movie>
     // Implicit return
     override fun getItemCount() = movies.size
 
-    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView),
+        View.OnClickListener {
         private val ivPoster = itemView.findViewById<ImageView>(R.id.ivPoster)
         private val tvTitle = itemView.findViewById<TextView>(R.id.tvTitle)
         private val tvOverview = itemView.findViewById<TextView>(R.id.tvOverview)
+
+        init {
+            itemView.setOnClickListener(this)
+        }
+
         fun bind(movie: Movie) {
             var image = movie.posterImageUrl
             val orientation = context.resources.configuration.orientation
@@ -48,6 +57,17 @@ class MovieAdapter(private val context: Context, private val movies: List<Movie>
             tvTitle.text = movie.title
             tvOverview.text = movie.overview
             Glide.with(context).load(image).placeholder(R.drawable.default_image).into(ivPoster)
+        }
+
+        override fun onClick(v: View?) {
+            // Notify the particular movie clicked
+            val movie = movies[adapterPosition]
+            // Toast.makeText(context, movie.title, Toast.LENGTH_SHORT).show()
+            // Use intents to navigate to new activity
+            val intent = Intent(context, DetailActivity::class.java)
+            // intent.putExtra("movie")
+            intent.putExtra(MOVIE_EXTRA, movie)
+            context.startActivity(intent)
         }
     }
 }
